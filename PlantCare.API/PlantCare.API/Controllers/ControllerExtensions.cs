016f1a16626923522bc0c522c3b9802ddbe1de72
ArekStasko <68238084+ArekStasko@users.noreply.github.com>
2023-08-29 20:26:49 +0200

    Feature/prepare error handling (#4)
    * Update Todos README.md
    
    * Create docker-image.yml
    
    * Configure LanguageExt
    
    * Add stylecop configuration
    
    * Delete .github/workflows directory
    
    * Delete PlantCare.API/.idea/.idea.PlantCare.API/.idea directory
    
    * Delete PlantCare.API/PlantCare.API.DataAccess/bin/Debug/net7.0 directory
    
    * Delete PlantCare.API/PlantCare.API.DataAccess/obj directory
    
    * Delete PlantCare.API/PlantCare.API.Services/bin/Debug/net7.0 directory
    
    * Delete PlantCare.API/PlantCare.API.Services/obj directory
    
    * Delete PlantCare.API/PlantCare.API/bin/Debug/net7.0 directory
    
    * Delete PlantCare.API/PlantCare.API/obj directory

---
+++ PlantCare.API/PlantCare.API/Controllers/ControllerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using LanguageExt.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlantCare.API.Controllers;
+
+public static class ControllerExtensions
+{
+    public static IActionResult ToOk<TResult, TContract>(this Result<TResult> result, Func<TResult, TContract> mapper)
+    {
+        return result.Match<IActionResult>(obj =>
+        {
+            var response = mapper(obj);
+            return new OkObjectResult(response);
+
+        }, exception =>
+        {
+            if (exception is ValidationException validationException)
+            {
+                return new BadRequestObjectResult(validationException);
+            }
+
+            return new StatusCodeResult(500);
+        });
+    }
+}
