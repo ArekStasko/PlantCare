7e6be8b871b171fffa4ac929d2088d2e63183328
ArekStasko <68238084+ArekStasko@users.noreply.github.com>
2023-09-01 21:30:45 +0200

    Develop (#8)
    * Add docker compose and move dockerfile up (#1)
    
    * Add Plant and Record models (#2)
    
    * Feature/add serilog configuration (#3)
    
    * Add Serilog and Seq packages
    
    * Add serilog and seq configuration
    
    * Add seq service to docker compose
    
    * Add new tasks to Todos.md
    
    * Delete PlantCare.API/.idea/.idea.PlantCare.API/.idea directory
    
    * Delete PlantCare.API/PlantCare.API.DataAccess/obj directory
    
    * Delete PlantCare.API/PlantCare.API.DataAccess/bin/Debug/net7.0 directory
    
    * Delete PlantCare.API/PlantCare.API.Services/obj directory
    
    * Delete PlantCare.API/PlantCare.API.Services/bin/Debug/net7.0 directory
    
    * Delete PlantCare.API/PlantCare.API/bin/Debug/net7.0 directory
    
    * Delete PlantCare.API/PlantCare.API/obj directory
    
    * Feature/prepare error handling (#4)
    
    * Update Todos README.md
    
    * Create docker-image.yml
    
    * Configure LanguageExt
    
    * Add stylecop configuration
    
    * Delete .github/workflows directory
    
    * Delete PlantCare.API/.idea/.idea.PlantCare.API/.idea directory
    
    * Delete PlantCare.API/PlantCare.API.DataAccess/bin/Debug/net7.0 directory
    
    * Delete PlantCare.API/PlantCare.API.DataAccess/obj directory
    
    * Delete PlantCare.API/PlantCare.API.Services/bin/Debug/net7.0 directory
    
    * Delete PlantCare.API/PlantCare.API.Services/obj directory
    
    * Delete PlantCare.API/PlantCare.API/bin/Debug/net7.0 directory
    
    * Delete PlantCare.API/PlantCare.API/obj directory
    
    * Configure MediatR (#7)
    
    * Add MediatR packages
    
    * Add requests and handlers directory with IHttpRequest.cs
    
    * Configure MediatR
    
    * Add MediatR dependency injection
    
    * Add gitignore
    
    * Change repo name to lowercase

---
+++ PlantCare.API/PlantCare.API.DataAccess/Models/Record/Record.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlantCare.API.DataAccess.Models.Record;
+
+public class Record : IRecord
+{
+    [Key]
+    public int PlantId { get; set; }
+    
+    [Required]
+    public byte MoistureLevel { get; set; }
+    
+    [Required]
+    public TimeSpan IrrigationDate { get; set; }
+}
