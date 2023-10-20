8c49e26d899867c6aee258d5967f42745c5404a5
arekstasko <arek.stasko@gmail.com>
2023-10-20 15:30:50 +0200

    Add prototype of dashboard table

--- plantcare.ui/src/pages/Dashboard/Dashboard.tsx
+++ plantcare.ui/src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,7 @@
-import {Box, Skeleton, Typography} from "@mui/material";
+import {Accordion, AccordionDetails, AccordionSummary, Box, Skeleton, Typography} from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useGetPlacesQuery } from "../../common/slices/getPlaces/getPlaces";
+import React from "react";
 
 const Dashboard = () => {
     const {data, isLoading} = useGetPlacesQuery();
@@ -11,9 +13,49 @@ const Dashboard = () => {
                 <Skeleton />
             </Box>
         ) : (
-            <Box>
-                <Typography>Test</Typography>
-            </Box>
+            data ? (
+                <Box>
+                    {
+                        data.map(place => (
+                            <Accordion key={place.id}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    <Typography>{place.name}</Typography>
+                                </AccordionSummary>
+                                {
+                                    place.plants ? (
+                                        place!.plants.map(plant => (
+                                            <AccordionDetails>
+                                                <Typography>
+                                                    {plant.name}
+                                                </Typography>
+                                                <Typography>
+                                                    {plant.description}
+                                                </Typography>
+                                            </AccordionDetails>
+                                        ))
+                                        ) : (
+                                        <AccordionDetails>
+                                            <Typography>
+                                                There is no plants
+                                            </Typography>
+                                        </AccordionDetails>
+                                    )
+                                }
+                            </Accordion>
+                        ))
+                    }
+                </Box>
+            ) : (
+                <Box>
+                    <Typography>
+                        For now you dont have any data
+                    </Typography>
+                </Box>
+            )
         )
     )
 }
