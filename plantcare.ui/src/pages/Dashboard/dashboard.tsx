5e567131e128982799d67e1cd61d10e373db4942
arekstasko <arek.stasko@gmail.com>
2023-10-24 20:44:31 +0200

    Separate CustomBackdrop

--- plantcare.ui/src/pages/Dashboard/dashboard.tsx
+++ plantcare.ui/src/pages/Dashboard/dashboard.tsx
@@ -2,82 +2,78 @@ import React from "react";
 import {
     Accordion,
     AccordionDetails,
-    AccordionSummary, Backdrop,
+    AccordionSummary,
     Box,
-    CircularProgress,
-    Skeleton,
     Typography
 } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useGetPlacesQuery } from "../../common/slices/getPlaces/getPlaces";
 import styles  from './dashboard.styles';
+import CustomBackdrop from "../../common/compontents/customBackdrop/backdrop";
 
 const Dashboard = () => {
     const [currentAccordion, setCurrentAccordion] = React.useState<number>();
-    const {data, isLoading} = useGetPlacesQuery();
+    const {data, isLoading : placesLoading} = useGetPlacesQuery();
 
 
     return(
-        isLoading ? (
-            <Backdrop
-                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                open={isLoading}
-            >
-                <CircularProgress color="inherit" />
-            </Backdrop>
-        ) : (
+        <Box>
+            isLoading ? (
+                <CustomBackdrop isLoading={placesLoading} />
+            ) : (
             data ? (
-                <Box>
-                    {
-                        data.map(place => (
-                            <Accordion
-                                expanded={currentAccordion == place.id}
-                                onChange={e => {
-                                    if(currentAccordion == place.id) setCurrentAccordion(undefined)
-                                    else setCurrentAccordion(place.id)
-                                }}
-                                key={place.id}
-                                disableGutters
+            <Box>
+                {
+                    data!.map(place => (
+                        <Accordion
+                            expanded={currentAccordion == place.id}
+                            onChange={e => {
+                                if(currentAccordion == place.id) setCurrentAccordion(undefined)
+                                else setCurrentAccordion(place.id)
+                            }}
+                            key={place.id}
+                            disableGutters
+                        >
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
                             >
-                                <AccordionSummary
-                                    expandIcon={<ExpandMoreIcon />}
-                                    aria-controls="panel1a-content"
-                                    id="panel1a-header"
-                                >
-                                    <Typography>{place.name}</Typography>
-                                </AccordionSummary>
-                                {
-                                    place.plants ? (
-                                        place!.plants.map(plant => (
-                                            <AccordionDetails>
-                                                <Typography>
-                                                    {plant.name}
-                                                </Typography>
-                                                <Typography>
-                                                    {plant.description}
-                                                </Typography>
-                                            </AccordionDetails>
-                                        ))
-                                        ) : (
+                                <Typography>{place.name}</Typography>
+                            </AccordionSummary>
+                            {
+                                place.plants ? (
+                                    place!.plants.map(plant => (
                                         <AccordionDetails>
                                             <Typography>
-                                                There is no plants
+                                                {plant.name}
+                                            </Typography>
+                                            <Typography>
+                                                {plant.description}
                                             </Typography>
                                         </AccordionDetails>
-                                    )
-                                }
-                            </Accordion>
-                        ))
-                    }
-                </Box>
+                                    ))
+                                ) : (
+                                    <AccordionDetails>
+                                        <Typography>
+                                            There is no plants
+                                        </Typography>
+                                    </AccordionDetails>
+                                )
+                            }
+                        </Accordion>
+                    ))
+                }
+            </Box>
             ) : (
-                <Box>
-                    <Typography>
-                        For now you dont have any data
-                    </Typography>
-                </Box>
+            <Box>
+                <Typography>
+                    For now you dont have any data
+                </Typography>
+            </Box>
             )
-        )
+            )
+        </Box>
     )
 }
 
