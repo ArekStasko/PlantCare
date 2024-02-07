13cd294684c3c7f882e9f38c8e6bb544ae20cdf5
arekstasko <arek.stasko@gmail.com>
2024-02-07 16:50:42 +0100

    Add consumer registration

--- PlantCare.API/PlantCare.MessageBroker/Extensions.cs
+++ PlantCare.API/PlantCare.MessageBroker/Extensions.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using PlantCare.MessageBroker.Consumer;
+using PlantCare.MessageBroker.Messages;
 using PlantCare.MessageBroker.Providers.ChannelProvider;
 using PlantCare.MessageBroker.Providers.ConnectionProvider;
 using PlantCare.MessageBroker.Providers.QueueChannelProvider;
@@ -9,6 +11,12 @@ namespace PlantCare.MessageBroker;
 
 public static class Extensions
 {
+    public static void AddQueueMessageConsumer<TMessageConsumer, TQueueMessage>(this IServiceCollection services) where TMessageConsumer : IQueueConsumer<TQueueMessage> where TQueueMessage : class, IQueueMessage
+    {
+        services.AddScoped(typeof(TMessageConsumer));
+        services.AddScoped<IQueueConsumerHandler<TMessageConsumer, TQueueMessage>, QueueConsumerHandler<TMessageConsumer, TQueueMessage>>();
+        services.AddHostedService<QueueConsumerRegistratorService<TMessageConsumer, TQueueMessage>>();
+    }
     public static void AddMessageBroker(this IServiceCollection services)
     {
         services.AddSingleton<IAsyncConnectionFactory>(provider =>
