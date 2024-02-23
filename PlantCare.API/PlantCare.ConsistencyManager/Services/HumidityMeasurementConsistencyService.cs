9c02cf4aa79a426f2874c2476b3fbbba5771b605
arekstasko <arek.stasko@gmail.com>
2024-02-23 20:12:50 +0100

    Add humidity measurement consistency service implementation

--- PlantCare.API/PlantCare.ConsistencyManager/Services/HumidityMeasurementConsistencyService.cs
+++ PlantCare.API/PlantCare.ConsistencyManager/Services/HumidityMeasurementConsistencyService.cs
@@ -1,19 +1,38 @@
+using AutoMapper;
 using Microsoft.Extensions.Logging;
 using PlantCare.MessageBroker.Consumer;
 using PlantCare.MessageBroker.Messages;
+using PlantCare.Persistance.ReadDataManager.Interfaces;
 
 namespace PlantCare.ConsistencyManager.Services;
 
 public class HumidityMeasurementConsistencyService : IQueueConsumer<HumidityMeasurement>
 {
+    private readonly IHumidityMeasurementReadContext _context;
+    private readonly IMapper _mapper;
     private readonly ILogger<HumidityMeasurementConsistencyService> _logger;
 
-    public HumidityMeasurementConsistencyService(ILogger<HumidityMeasurementConsistencyService> logger)
+    public HumidityMeasurementConsistencyService(IHumidityMeasurementReadContext context, IMapper mapper, ILogger<HumidityMeasurementConsistencyService> logger)
     {
+        _context = context;
+        _mapper = mapper;
         _logger = logger;
     }
-    public Task ConsumeAsync(HumidityMeasurement message)
+    public async Task ConsumeAsync(HumidityMeasurement message)
     {
-        return Task.CompletedTask;
+        switch (message.Action)
+        {
+            case ActionType.Add:
+            {
+                var humidityMeasurement = _mapper.Map<PlantCare.Domain.Models.HumidityMeasurement.HumidityMeasurement>(message.HumidityMeasurementData);
+                await _context.HumidityMeasurements.AddAsync(humidityMeasurement);
+                return;
+            }
+            default:
+            {
+                _logger.LogError("Humidity Measurement Consistency service executes for not existing action: {action}", message.Action);
+                return;
+            }
+        }
     }
 }
