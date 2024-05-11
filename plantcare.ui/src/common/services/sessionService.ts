9a80aaa48a2e0fec934b10c10eb3c040022e31f4
arekstasko <arek.stasko@gmail.com>
2024-05-11 20:32:07 +0200

    remove wrong implemented sessionService

--- plantcare.ui/src/common/services/sessionService.ts
+++ plantcare.ui/src/common/services/sessionService.ts
@@ -1,35 +1,3 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import RoutingConstants from '../../app/routing/routingConstants';
-import { useCheckTokenQuery } from '../slices/checkTokenExpiration/checkTokenExpiration';
-import { GetUserData } from './CookieService';
-
-const useBackgroundService = () => {
-  const navigate = useNavigate();
-  const [isTokenValid, setIsTokenValid] = useState<boolean>(true);
-
-  useEffect(() => {
-    const bgService = setInterval(async () => {
-      const userData = GetUserData();
-      if (!userData) {
-        clearInterval(bgService);
-        navigate(RoutingConstants.auth);
-        return;
-      }
-
-      const { data: isValid } = useCheckTokenQuery(userData.token);
-      setIsTokenValid(isValid ?? false);
-
-      if (!isValid) {
-        clearInterval(bgService);
-        navigate(RoutingConstants.auth);
-      }
-    }, 120000);
-
-    return () => clearInterval(bgService);
-  }, [navigate]);
-
-  return isTokenValid;
-};
+const useBackgroundService = () => {};
 
 export default useBackgroundService;
