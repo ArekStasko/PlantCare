f6fcc234122e1566ea27a1416ff60f3a0a65c53d
arekstasko <arek.stasko@gmail.com>
2024-06-03 19:27:11 +0200

    Add userId to controllers

--- PlantCare.API/PlantCare.API/Controllers/PlaceController.cs
+++ PlantCare.API/PlantCare.API/Controllers/PlaceController.cs
@@ -40,6 +40,7 @@ public class PlaceController : ControllerBase
     {
         _logger.LogInformation("Delete place controller method start processing");
         var deletePlaceCommand = _mapper.Map<DeletePlaceCommand>(id);
+        deletePlaceCommand.UserId = userId;
         var result = await _mediator.Send(deletePlaceCommand);
         _logger.LogInformation("Delete place controller method ends processing");
         return result.ToOk();
@@ -59,10 +60,14 @@ public class PlaceController : ControllerBase
     [HttpGet(Name = "[controller]/get")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<IPlant>))]
     [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(Exception))]
-    public async ValueTask<IActionResult> Get()
+    public async ValueTask<IActionResult> Get([FromQuery] int userId)
     {
         _logger.LogInformation("GetAll places controller method start processing");
-        var result = await _mediator.Send(new GetPlacesQuery());
+        var getPlacesQuery = new GetPlacesQuery()
+        {
+            UserId = userId
+        };
+        var result = await _mediator.Send(getPlacesQuery);
         _logger.LogInformation("GetAll places controller method ends processing");
         return result.ToOk();
     }
--- PlantCare.API/PlantCare.Commands/Commands/Place/UpdatePlaceCommand.cs
+++ PlantCare.API/PlantCare.Commands/Commands/Place/UpdatePlaceCommand.cs
@@ -3,5 +3,6 @@ namespace PlantCare.Commands.Commands.Place;
 public record UpdatePlaceCommand : IHttpPostCommand
 {
     public int Id { get; set; }
+    public int UserId { get; set; }
     public string Name { get; set; }
 };
