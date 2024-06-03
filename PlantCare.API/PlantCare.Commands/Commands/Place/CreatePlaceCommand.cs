33fb4f3d173ee6c01860ead2eac1e48059290638
arekstasko <arek.stasko@gmail.com>
2024-06-03 19:48:22 +0200

    Update postman collection

--- PlantCare.API/PlantCare.Commands/Commands/Place/CreatePlaceCommand.cs
+++ PlantCare.API/PlantCare.Commands/Commands/Place/CreatePlaceCommand.cs
@@ -3,5 +3,5 @@ namespace PlantCare.Commands.Commands.Place;
 public record CreatePlaceCommand : IHttpPostCommand
 {
     public string Name { get; set; }
-    public string UserId { get; set; }
+    public int UserId { get; set; }
 };
--- PlantCare.API/PlantCare.Domain/Dto/PlaceDto.cs
+++ PlantCare.API/PlantCare.Domain/Dto/PlaceDto.cs
@@ -3,5 +3,6 @@ namespace PlantCare.Domain.Dto;
 public class PlaceDto
 {
     public int Id { get; set; }
+    public int UserId { get; set; }
     public string Name { get; set; }
 }
