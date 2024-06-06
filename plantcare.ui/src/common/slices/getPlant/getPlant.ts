07fd8d10a19ee29618e05f580a3e28fbc4d11ae4
arekstasko <arek.stasko@gmail.com>
2024-06-06 21:10:32 +0200

    Add userId to plant slices

--- plantcare.ui/src/common/slices/getPlant/getPlant.ts
+++ plantcare.ui/src/common/slices/getPlant/getPlant.ts
@@ -1,10 +1,11 @@
 import emptyApi from '../../../app/api/emptyApi';
 import { Plant } from '../../models/Plant';
+import { GetPlantData } from './getPlantData';
 
 export const getPlantApi = emptyApi.injectEndpoints({
   endpoints: (build) => ({
-    GetPlant: build.query<Plant, string>({
-      query: (plantId: string) => `/plants/getById?id=${plantId}`
+    GetPlant: build.query<Plant, GetPlantData>({
+      query: ({ plantId, userId }: GetPlantData) => `/plants/getById?id=${plantId}&userId=${userId}`
     })
   }),
   overrideExisting: false
---
+++ plantcare.ui/src/common/slices/getPlant/getPlantData.ts
@@ -0,0 +1,4 @@
+export type GetPlantData = {
+  plantId: string;
+  userId: number;
+};
