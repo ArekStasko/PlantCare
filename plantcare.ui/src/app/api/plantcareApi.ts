ee24a86ed7e0497596c198086e458b3260156087
ArekStasko <arek.stasko@gmail.com>
2024-08-07 19:48:50 +0200

    Add idp-client package

--- plantcare.ui/src/app/api/plantcareApi.ts
+++ plantcare.ui/src/app/api/plantcareApi.ts
@@ -12,7 +12,6 @@ const plantcareApiBaseQuery = async (args: (string | FetchArgs), api: BaseQueryA
       return headers;
     },
   });
-
   const result = await rawBaseQuery(args, api, extraOptions);
   if (result['error'] && result['error'].status === 401) api.dispatch(logout())
   return result;
