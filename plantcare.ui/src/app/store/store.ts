05ef8616defb23597c5cdf303b81ce3efc05cb38
ArekStasko <arek.stasko@gmail.com>
2024-08-11 17:35:36 +0200

    Remove routing slice

--- plantcare.ui/src/app/store/store.ts
+++ plantcare.ui/src/app/store/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import plantcareApi from '../api/plantcareApi';
-import routeReducer from '../../common/RTK/routeSlice/routeSlice';
 
 const store = configureStore({
   reducer: {
     [plantcareApi.reducerPath]: plantcareApi.reducer,
-    route: routeReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(plantcareApi.middleware)
--- plantcare.ui/src/index.tsx
+++ plantcare.ui/src/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './app/store/store';
+import RoutingConstants from "./app/routing/routingConstants";
+import IdpClient from 'identity-provider-client'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <IdpClient clientStore={store} authBaseRoute={RoutingConstants.authBasic}>
       <App />
-    </Provider>
+    </IdpClient>
   </React.StrictMode>
 );
 
