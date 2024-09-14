e917af63f02b56438395679e94c8f6967992e7ef
ArekStasko <arek.stasko@gmail.com>
2024-09-14 15:15:43 +0200

    Add full write repo tests

--- PlantCare.API/PlantCare.Tests/Mocks/Services.cs
+++ PlantCare.API/PlantCare.Tests/Mocks/Services.cs
@@ -1,10 +1,12 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Moq;
-using PlantCare.Domain.Models.HumidityMeasurement;
-using PlantCare.Domain.Models.Module;
 using PlantCare.Persistance.WriteDataManager.Interfaces;
 using MockQueryable.Moq;
+using PlantCare.Domain.Enums;
+using PlantCare.Domain.Models.Place;
+using HumidityMeasurement = PlantCare.Domain.Models.HumidityMeasurement.HumidityMeasurement;
+using Module = PlantCare.Domain.Models.Module.Module;
+using Plant = PlantCare.Domain.Models.Plant.Plant;
 
 namespace PlantCare.Tests.Mocks;
 
@@ -19,6 +21,31 @@ public static class Services
             UserId = 1
         }
     }.AsQueryable().BuildMockDbSet();
+
+    public static Mock<DbSet<Plant>> PlantDb { get; } = new List<Plant>()
+    {
+        new()
+        {
+            Id = 1,
+            UserId = 1,
+            Description = "",
+            ModuleId = Guid.NewGuid(),
+            PlaceId = 1, 
+            Type = PlantType.Decorative
+        }
+    }.AsQueryable().BuildMockDbSet();
+
+    public static Mock<DbSet<Place>> PlaceDb { get; } = new List<Place>()
+    {
+        new()
+        {
+            Id = 1,
+            UserId = 1,
+            Name = ""
+        }
+    }.AsQueryable().BuildMockDbSet();
     public static Mock<IHumidityMeasurementWriteContext> HumidityMeasurementWriteContext { get; } = new();
     public static Mock<IModuleWriteContext> ModuleWriteContext { get; } = new();
+    public static Mock<IPlantWriteContext> PlantWriteContext { get; } = new();
+    public static Mock<IPlaceWriteContext> PlaceWriteContext { get; } = new();
 }
