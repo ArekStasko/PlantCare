c923cd5a2a74824076c8b3aad44966ff300047e0
ArekStasko <arek.stasko@gmail.com>
2025-01-03 22:03:50 +0100

    Add get all modules

--- PlantCare.API/PlantCare.Persistance.ReadDataManager/Repositories/Interfaces/IReadModuleRepository.cs
+++ PlantCare.API/PlantCare.Persistance.ReadDataManager/Repositories/Interfaces/IReadModuleRepository.cs
@@ -5,6 +5,7 @@ namespace PlantCare.Persistance.ReadDataManager.Repositories.Interfaces;
 
 public interface IReadModuleRepository
 {
+    ValueTask<Result<IReadOnlyCollection<IModule>>> Get();
     ValueTask<Result<IReadOnlyCollection<IModule>>> Get(int userId);
 
     ValueTask<Result<IModule>> Get(int userId, int id);
