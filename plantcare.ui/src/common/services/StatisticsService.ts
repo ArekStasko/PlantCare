9a2430bd86ebb963f5bdc49521117e3f6a5073c6
ArekStasko <arek.stasko@gmail.com>
2025-01-30 22:12:45 +0100

    Add proper chart

--- plantcare.ui/src/common/services/StatisticsService.ts
+++ plantcare.ui/src/common/services/StatisticsService.ts
@@ -2,24 +2,20 @@ import { HumidityMeasurement } from '../models/HumidityMeasurement';
 import { HumidityData, HumidityStatistics } from '../models/HumidityStatistics';
 import DateService from './DateService';
 
-const convertDataToStatistics = (
-  data: HumidityMeasurement[]
-): { id: string; data: { x: string; y: number }[] }[] => {
-  const humidityStatistics = [
-    {
-      id: 'Humidity Measurements',
-      data: data.map((measurement) => {
-        return {
-          x: DateService.convertDatesToStrings(measurement.date),
-          y: measurement.humidity
-        } as HumidityData;
-      })
-    }
-  ] as HumidityStatistics[];
+const getHumidityMeasurementTime = (data: HumidityMeasurement[]): string[] => data.map(c => {
+  const date = new Date(c.date);
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
 
-  return humidityStatistics as { id: string; data: { x: string; y: number }[] }[];
-};
+  const formattedHours = hours < 10 ? `0${hours}` : `${hours}`;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+
+  return `${formattedHours}:${formattedMinutes}`;
+})
+
+const getHumidityMeasurementValues = (data: HumidityMeasurement[]): number[] => data.map(c => c.humidity)
 
 export default {
-  convertDataToStatistics
+  getHumidityMeasurementTime,
+  getHumidityMeasurementValues
 };
--- plantcare.ui/src/pages/Statistics/components/measurementsChart.tsx
+++ plantcare.ui/src/pages/Statistics/components/measurementsChart.tsx
@@ -3,6 +3,7 @@ import { ResponsiveLine } from '@nivo/line';
 import StatisticsService from '../../../common/services/StatisticsService';
 import React, { useEffect } from 'react';
 import { HumidityMeasurement } from '../../../common/models/HumidityMeasurement';
+import { BarChart } from "@mui/x-charts";
 
 interface MeasurementsChartProps {
   humidityMeasurements: HumidityMeasurement[];
@@ -15,64 +16,10 @@ export const MeasurementsChart = (props: MeasurementsChartProps) => {
   };
 
   return (
-    <Box sx={{ height: '100%', width: '100%' }}>
-      <ResponsiveLine
-        data={StatisticsService.convertDataToStatistics(props.humidityMeasurements!)}
-        margin={{ top: 40, right: 40, bottom: 40, left: 50 }}
-        xScale={{ type: 'point' }}
-        enableArea={true}
-        areaBaselineValue={getSmallestRecord()}
-        yScale={{
-          type: 'linear',
-          min: 'auto',
-          max: 'auto',
-          stacked: true,
-          reverse: false
-        }}
-        yFormat=" >-.2f"
-        axisBottom={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'Measurement Time',
-          legendOffset: 36,
-          legendPosition: 'middle'
-        }}
-        axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'Humidity',
-          legendOffset: -40,
-          legendPosition: 'middle'
-        }}
-        theme={{
-          axis: {
-            ticks: {
-              line: {
-                stroke: 'white'
-              },
-              text: {
-                fill: 'white'
-              }
-            }
-          },
-          crosshair: {
-            line: {
-              stroke: '#fff',
-              strokeWidth: 2,
-              strokeOpacity: 0.35
-            }
-          }
-        }}
-        pointSize={10}
-        pointColor={{ theme: 'background' }}
-        pointBorderWidth={2}
-        pointBorderColor={{ from: 'serieColor' }}
-        pointLabelYOffset={-12}
-        useMesh={true}
-      />
-    </Box>
+    <BarChart
+      xAxis={[{ scaleType: 'band', data: StatisticsService.getHumidityMeasurementTime(props.humidityMeasurements) }]}
+      series={[{ data: StatisticsService.getHumidityMeasurementValues(props.humidityMeasurements) }]}
+    />
   );
 };
 
