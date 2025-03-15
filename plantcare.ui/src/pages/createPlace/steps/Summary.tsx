d195b6a0ca3789cb25d23f01bd95d338c37fb2b7
ArekStasko <arek.stasko@gmail.com>
2025-03-15 17:44:37 +0100

    Add summary styles

--- plantcare.ui/src/pages/createPlace/steps/Summary.tsx
+++ plantcare.ui/src/pages/createPlace/steps/Summary.tsx
@@ -1,15 +1,39 @@
-import { Box, Typography } from "@mui/material";
-import { WizardStepProps } from "../../../common/wizard/interfaces";
+import { Typography } from "@mui/material";
+import { buttonAction, WizardStepProps } from "../../../common/wizard/interfaces";
 import { CreatePlaceContext } from "../interfaces";
-
+import { WizardStep } from "../../../common/wizard/components/wizardStep/WizardStep";
 
 const Summary = ({wizardController}: WizardStepProps<CreatePlaceContext>) => {
+
+  const nextButton = {
+    onClick: () => wizardController.goToNextStep(),
+    isDisabled: false,
+    title: "Submit"
+  } as buttonAction
+
+  const cancelButton = {
+    onClick: () => wizardController.goToNextStep(),
+    isDisabled: false,
+    title: "Cancel"
+  } as buttonAction
+
+  const backButton = {
+    onClick: () => wizardController.goToPreviousStep(),
+    isDisabled: false,
+    title: "Back"
+  } as buttonAction
+
   return (
-    <Box>
+    <WizardStep
+      nextButton={nextButton}
+      cancelButton={cancelButton}
+      backButton={backButton}
+      title="Details"
+    >
       <Typography>
         Create Place Summary
       </Typography>
-    </Box>
+    </WizardStep>
   )
 }
 
