fd45d806ccae49aa82ca2d7249eb5f80ee588339
ArekStasko <arek.stasko@gmail.com>
2025-04-26 21:11:32 +0200

    remove steps from dialog

--- plantcare.ui/src/pages/updatePlant-new/UpdatePlant.tsx
+++ plantcare.ui/src/pages/updatePlant-new/UpdatePlant.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Dialog } from '@mui/material';
-import UpdatePlantMenu from './updatePlantMenu/UpdatePlantMenu';
 
 interface UpdatePlantProps {
   setOpenDialog: React.Dispatch<React.SetStateAction<boolean>>;
@@ -8,12 +7,6 @@ interface UpdatePlantProps {
 }
 
 const UpdatePlant = ({ setOpenDialog, openDialog }: UpdatePlantProps) => {
-  const steps = [
-    {
-      order: 1,
-      component: () => <UpdatePlantMenu />
-    }
-  ];
 
   return <Dialog open={openDialog} onClose={() => setOpenDialog(false)}></Dialog>;
 };
