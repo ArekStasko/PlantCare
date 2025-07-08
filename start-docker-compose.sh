362ceef213a823caae1d0d3aeec12a0b925a896a
ArekStasko <arek.stasko@gmail.com>
2025-07-08 19:56:21 +0200

    Add start-docker-compose script file

---
+++ start-docker-compose.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Purpose of this script is to run the plantcare application and its resources in a docker environment,
+# the script should be placed above the database, idp, and plantcare api folders
