5bd6aa62d52d119eb3420e0fe3249625c2fd5365
ArekStasko <arek.stasko@gmail.com>
2025-10-25 21:52:10 +0200

    Fix redux issues temporarly remove useSession

--- plantcare.ui/src/pages/expirationBanner/expirationBanner.tsx
+++ plantcare.ui/src/pages/expirationBanner/expirationBanner.tsx
@@ -1,8 +1,30 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import { ExpirationBannerInterface } from "identity-provider-client";
 
+const ExpirationBanner: React.FC<ExpirationBannerInterface> = ({open, onClose, onRefresh, onLogout}) => {
 
-const ExpirationBanner = () => {
-
-  return();
+  return(
+    <Dialog
+      open={open}
+      onClose={onClose}
+    >
+      <DialogTitle>
+        {"Login session has expired."}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          Click logout button to go to login page.
+          If you want to stay login click refresh button.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onRefresh}>Refresh</Button>
+        <Button onClick={onLogout} autoFocus>
+          Logout
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
 
 export default ExpirationBanner;
