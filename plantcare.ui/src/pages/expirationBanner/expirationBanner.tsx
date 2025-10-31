1e2f9400ffdd62b147b1ecab0a6624bd67af219d
ArekStasko <arek.stasko@gmail.com>
2025-10-31 23:57:23 +0100

    Add countdownTimer

--- plantcare.ui/src/pages/expirationBanner/expirationBanner.tsx
+++ plantcare.ui/src/pages/expirationBanner/expirationBanner.tsx
@@ -1,5 +1,6 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 import { ExpirationBannerInterface } from "identity-provider-client";
+import {CountdownTimer} from "./CountdownTimer";
 
 const ExpirationBanner: React.FC<ExpirationBannerInterface> = ({open, onClose, onRefresh, onLogout}) => {
 
@@ -16,12 +17,13 @@ const ExpirationBanner: React.FC<ExpirationBannerInterface> = ({open, onClose, o
           Click logout button to go to login page.
           If you want to stay login click refresh button.
         </DialogContentText>
+        <CountdownTimer timeToCountDown={300} onTimeOut={() => onLogout()} />
       </DialogContent>
       <DialogActions>
-        <Button onClick={onRefresh}>Refresh</Button>
-        <Button onClick={onLogout} autoFocus>
+        <Button variant="outlined" onClick={onLogout} autoFocus>
           Logout
         </Button>
+        <Button variant='contained' onClick={onRefresh}>Stay</Button>
       </DialogActions>
     </Dialog>
   );
