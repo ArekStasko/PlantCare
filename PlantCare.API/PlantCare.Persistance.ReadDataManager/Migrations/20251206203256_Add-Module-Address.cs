b8f9a3e71b3cf307feaf05fb152297981e266cd5
ArekStasko <arek.stasko@gmail.com>
2025-12-06 21:34:24 +0100

    Migrations

---
+++ PlantCare.API/PlantCare.Persistance.ReadDataManager/Migrations/20251206203256_Add-Module-Address.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlantCare.Persistance.ReadDataManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddModuleAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Modules",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Modules");
+        }
+    }
+}
