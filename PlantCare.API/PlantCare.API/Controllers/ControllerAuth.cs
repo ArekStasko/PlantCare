39f7ae39b8e0f7250fe26788ea3f8294119c55a7
ArekStasko <arek.stasko@gmail.com>
2026-01-08 20:54:56 +0100

    Adjust authorization for module token

--- PlantCare.API/PlantCare.API/Controllers/ControllerAuth.cs
+++ PlantCare.API/PlantCare.API/Controllers/ControllerAuth.cs
@@ -5,16 +5,37 @@ namespace PlantCare.API.Controllers;
 public class ControllerAuth : ControllerBase
 {
     protected int UserId { get; private set; }
+    private readonly ILogger<ControllerAuth> _logger;
+
+    public ControllerAuth(ILogger<ControllerAuth> logger)
+    {
+        _logger = logger;
+    }
     
     public ControllerAuth(IHttpContextAccessor httpContextAccessor)
     {
+        string secretToken = Environment.GetEnvironmentVariable("secretToken");
+
+        var authHeader = httpContextAccessor.HttpContext.Request.Headers["Authorization"].FirstOrDefault();
+        if (!string.IsNullOrEmpty(authHeader) && authHeader.StartsWith("Bearer "))
+        {
+            var token = authHeader.Substring("Bearer ".Length).Trim();
+            _logger.LogInformation("Request contains bearer token : {token} - {authHeader}", token, authHeader);
+            if(token == secretToken)
+            {
+                _logger.LogInformation("Request authorized with secret token");
+                return;
+            }
+        }
+        
         if (httpContextAccessor.HttpContext.Items.TryGetValue("UserId", out var userId))
         {
+            _logger.LogInformation("Request authorized with IDP");
             UserId = (int)userId;
+            return;
         }
-        else
-        {
-            throw new UnauthorizedAccessException();
-        }
+        
+        _logger.LogInformation("Request is not authorized");
+        throw new UnauthorizedAccessException();
     }
 }
