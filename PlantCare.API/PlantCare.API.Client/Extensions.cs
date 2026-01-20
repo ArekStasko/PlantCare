1f5c37a5e54b7cf6667c8c100e0aba4052b09c85
ArekStasko <arek.stasko@gmail.com>
2026-01-20 19:50:57 +0100

    Plantcare.Api.Client project

---
+++ PlantCare.API/PlantCare.API.Client/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PlantCare.API.Client;
+
+public static class Extensions
+{
+    public static void ConfigureClient(this IServiceCollection services)
+    {
+        services.AddOpenApiDocument();
+    }
+}
