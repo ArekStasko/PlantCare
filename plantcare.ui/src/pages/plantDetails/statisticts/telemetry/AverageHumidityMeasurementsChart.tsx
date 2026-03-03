fa77d00dbd367619d6c62bd45780f3caaa71f862
ArekStasko <arek.stasko@gmail.com>
2026-03-03 20:10:49 +0100

    Date range pickers logic

--- plantcare.ui/src/pages/plantDetails/statisticts/telemetry/AverageHumidityMeasurementsChart.tsx
+++ plantcare.ui/src/pages/plantDetails/statisticts/telemetry/AverageHumidityMeasurementsChart.tsx
@@ -2,9 +2,35 @@ import { Box, CircularProgress, Typography } from '@mui/material';
 import { HumidityMeasurementsChartProps } from './interfaces';
 import styles from '../statistics.styles';
 import { DatePicker } from '@mui/x-date-pickers';
-import React from 'react';
+import React, { useState } from "react";
+import { Dayjs } from "dayjs";
+import dateService from "../../../../common/services/DateService";
+
+const enum DateType {
+  FROM,
+  TO,
+}
 
 const AverageHumidityMeasurementsChart = ({ moduleId }: HumidityMeasurementsChartProps) => {
+  const [fromDate, setFromDate] = useState<string | null>(null);
+  const [toDate, setToDate] = useState<string | null>(null);
+
+  const setDateValue = (value: Dayjs, type: DateType) => {
+    const correctDate = value.toDate();
+    const year = correctDate.getFullYear();
+    const month = correctDate.getMonth() + 1;
+    const day = correctDate.getDate();
+
+    if(type === DateType.FROM){
+      const fromDate = dateService.getStartOfGivenDay(year, month, day);
+      setFromDate(fromDate);
+      return;
+    }
+
+    const toDate = dateService.getEndOfGivenDay(year, month, day);
+    setToDate(toDate);
+  }
+
   return (
     <>
       <Box sx={styles.measurementsBar}>
@@ -15,14 +41,14 @@ const AverageHumidityMeasurementsChart = ({ moduleId }: HumidityMeasurementsChar
             disabled={false}
             disableFuture
             disableHighlightToday
-            onAccept={(value) => console.log('from date accept')}
+            onAccept={(value) => setDateValue(value as Dayjs, DateType.FROM)}
           />
           <DatePicker
             label="To Day"
-            disabled={false}
+            disabled={!fromDate}
             disableFuture
             disableHighlightToday
-            onAccept={(value) => console.log('to date accept')}
+            onAccept={(value) => setDateValue(value as Dayjs, DateType.TO)}
           />
         </Box>
       </Box>
--- plantcare.ui/src/pages/plantDetails/statisticts/telemetry/interfaces.ts
+++ plantcare.ui/src/pages/plantDetails/statisticts/telemetry/interfaces.ts
@@ -1,3 +1,4 @@
 export interface HumidityMeasurementsChartProps {
   moduleId: string;
 }
+
