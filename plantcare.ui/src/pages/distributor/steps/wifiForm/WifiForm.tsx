c9c7a608a2487d57f528ab122ddda613607dc13f
ArekStasko <arek.stasko@gmail.com>
2026-05-31 17:32:34 +0200

    distributor wifi step

--- plantcare.ui/src/pages/distributor/steps/wifiForm/WifiForm.tsx
+++ plantcare.ui/src/pages/distributor/steps/wifiForm/WifiForm.tsx
@@ -1,14 +1,43 @@
-import { Typography } from '@mui/material';
+import { Box, TextField, Typography } from "@mui/material";
 import { AddDistributorContext } from '../../interfaces';
 import { WizardStepProps } from '../../../../common/wizard/interfaces';
 import { WizardStep } from '../../../../common/wizard/components/wizardStep/WizardStep';
+import styles from "../../../addModule/steps/wifiForm/wifiForm.styles";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import validators from "../../../../common/services/Validators";
 
 const WifiForm = ({ wizardController }: WizardStepProps<AddDistributorContext>) => {
+  const methods = useForm({
+    mode: 'onChange',
+    resolver: yupResolver(validators.addModuleSchema),
+    defaultValues: {
+      wifiName: wizardController.context.wifiName ?? '',
+      wifiPassword: wizardController.context.wifiPassword ?? ''
+    }
+  });
+
+  const {
+    register,
+    getValues,
+    formState: { errors, isValid }
+  } = methods;
+
+  const onNext = () => {
+    wizardController.updateContext({
+      ...wizardController.context,
+      wifiName: getValues('wifiName'),
+      wifiPassword: getValues('wifiPassword')
+    });
+    wizardController.goToNextStep();
+  };
+
   return (
     <WizardStep
       nextButton={{
-        onClick: async () => console.log('wifi form submit'),
-        isDisabled: false,
+        onClick: onNext,
+        isDisabled: !isValid,
         title: 'Next'
       }}
       cancelButton={{
@@ -23,7 +52,30 @@ const WifiForm = ({ wizardController }: WizardStepProps<AddDistributorContext>) 
       }}
       title={'Wifi'}
     >
-      <Typography>Distributor wifi</Typography>
+      <Box sx={styles.container}>
+        <Typography sx={styles.subtitle} variant="h6">
+          Provide your WiFi network information
+        </Typography>
+        <TextField
+          sx={styles.textfield}
+          label="SSID"
+          id="wifiName"
+          error={!!errors.wifiName}
+          helperText={errors.wifiName && 'WiFi name is required'}
+          variant="filled"
+          {...register('wifiName')}
+        />
+        <TextField
+          sx={styles.textfield}
+          label="Password"
+          id="wifiPassword"
+          type="password"
+          error={!!errors.wifiPassword}
+          helperText={errors.wifiPassword && 'WiFi password is required'}
+          variant="filled"
+          {...register('wifiPassword')}
+        />
+      </Box>
     </WizardStep>
   );
 };
