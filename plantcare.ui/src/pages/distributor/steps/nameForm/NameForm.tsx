35bf491325ddc02aca89e303a53e0e733b02c3d8
ArekStasko <arek.stasko@gmail.com>
2026-05-31 17:38:41 +0200

    distributor name form step

--- plantcare.ui/src/pages/distributor/steps/nameForm/NameForm.tsx
+++ plantcare.ui/src/pages/distributor/steps/nameForm/NameForm.tsx
@@ -1,14 +1,48 @@
-import { Typography } from '@mui/material';
+import { Box, IconButton, TextField, Typography } from "@mui/material";
 import { AddDistributorContext } from '../../interfaces';
 import { WizardStepProps } from '../../../../common/wizard/interfaces';
 import { WizardStep } from '../../../../common/wizard/components/wizardStep/WizardStep';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import validators from "../../../../common/services/Validators";
+import { GetName } from "namee";
+import styles from "../../../addModule/steps/nameForm/nameForm.styles";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import React from "react";
 
 const NameForm = ({ wizardController }: WizardStepProps<AddDistributorContext>) => {
+  const methods = useForm({
+    mode: 'onChange',
+    resolver: yupResolver(validators.addModuleNameSchema),
+    defaultValues: {
+      distributorName: wizardController.context.distributorName ?? GetName()
+    }
+  });
+
+  const {
+    register,
+    setValue,
+    getValues,
+    trigger,
+    formState: { errors, isValid }
+  } = methods;
+
+  const resetName = () => {
+    setValue('distributorName', GetName());
+    trigger('distributorName');
+  };
+
   return (
     <WizardStep
       nextButton={{
-        onClick: async () => console.log('name form submit'),
-        isDisabled: false,
+        onClick: () => {
+          wizardController.updateContext({
+            ...wizardController.context,
+            distributorName: getValues('distributorName')
+          });
+          wizardController.goToNextStep();
+        },
+        isDisabled: !isValid,
         title: 'Next'
       }}
       cancelButton={{
@@ -23,7 +57,22 @@ const NameForm = ({ wizardController }: WizardStepProps<AddDistributorContext>) 
       }}
       title={'NameForm'}
     >
-      <Typography>Distributor name form</Typography>
+      <Box sx={styles.nameForm}>
+        <Typography variant="h6">Enter the name of the distributor</Typography>
+        <Box sx={styles.textFieldWrapper}>
+          <TextField
+            sx={styles.textfield}
+            id="moduleName"
+            variant="standard"
+            error={!!errors.moduleName}
+            helperText={errors?.moduleName?.message?.toString()}
+            {...register('distributorName')}
+          />
+          <IconButton sx={styles.iconButton} onClick={() => resetName()}>
+            <RefreshIcon />
+          </IconButton>
+        </Box>
+      </Box>
     </WizardStep>
   );
 };
