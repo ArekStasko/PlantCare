df5f5a2a66977f7b29f24512e34a52c039b4111b
ArekStasko <arek.stasko@gmail.com>
2026-09-13 15:23:22 +0200

    Implement water supply status with plant handler

--- PlantCare.API/PlantCare.Queries/Queries/WaterSupply/GetWaterSupplyByDistributorId.cs
+++ PlantCare.API/PlantCare.Queries/Queries/WaterSupply/GetWaterSupplyByDistributorId.cs
@@ -1,9 +1,10 @@
 using LanguageExt.Common;
 using MediatR;
+using PlantCare.Queries.Responses.Distributor;
 
 namespace PlantCare.Queries.Queries.WaterSupply;
 
-public class GetWaterSupplyByDistributorId : IRequest<Result<int>>
+public class GetWaterSupplyByDistributorId : IRequest<Result<WaterSupplyWithPlant>>
 {
     public int DistributorId { get; set; }
 }
--- PlantCare.API/PlantCare.Queries/QueryHandlers/DistributorQueryHandlers/GetWaterSupplyByDistributorIdHandler.cs
+++ PlantCare.API/PlantCare.Queries/QueryHandlers/DistributorQueryHandlers/GetWaterSupplyByDistributorIdHandler.cs
@@ -3,34 +3,44 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 using PlantCare.Persistance.ReadDataManager.Repositories.Interfaces;
 using PlantCare.Queries.Queries.WaterSupply;
+using PlantCare.Queries.Responses.Distributor;
 
 namespace PlantCare.Queries.QueryHandlers.DistributorQueryHandlers;
 
 public class GetWaterSupplyByDistributorIdHandler(
         IReadDistributorRepository repository,
         ILogger<GetWaterSupplyByDistributorIdHandler> logger
-    ) : IRequestHandler<GetWaterSupplyByDistributorId, Result<int>>
+    ) : IRequestHandler<GetWaterSupplyByDistributorId, Result<WaterSupplyWithPlant>>
 {
-    public async Task<Result<int>> Handle(GetWaterSupplyByDistributorId request, CancellationToken cancellationToken)
+    public async Task<Result<WaterSupplyWithPlant>> Handle(GetWaterSupplyByDistributorId request, CancellationToken cancellationToken)
     {
         try
         {
             var result = await repository.GetFirstWaterSupplyByDistributorId(request.DistributorId);
+            var waterSupplyWithPlant = new WaterSupplyWithPlant()
+            {
+                Id = -1,
+                PlantId = -1
+            };
             return result.Match(succ =>
             {
-                if (succ != null) return succ.PlantId;
-                return -1;
+                if (succ != null)
+                {
+                    waterSupplyWithPlant.Id = succ.Id;
+                    waterSupplyWithPlant.PlantId = succ.PlantId;
+                }
+                return new Result<WaterSupplyWithPlant>(waterSupplyWithPlant);
             }, err =>
             {
                 logger.LogError(
                     "Something went wrong while fetching water supply record from distributor repository: {e}", err);
-                return new Result<int>(err);
+                return new Result<WaterSupplyWithPlant>(err);
             });
         }
         catch (Exception e)
         {
             logger.LogError("Something went wrong while handling water supply status query: {e}", e);
-            return new Result<int>(e);
+            return new Result<WaterSupplyWithPlant>(e);
         }
     }
 }
